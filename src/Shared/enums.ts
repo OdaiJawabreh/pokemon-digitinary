c8c470d196cf8dffefcbe3ca84852b56610c2954
OdaiJawabreh <odaijawabreh.oj@gmail.com>
2023-12-09 23:25:03 +0300

    Add DTO for User Module and swagger

---
+++ src/Shared/enums.ts
@@ -0,0 +1,42 @@
+/* eslint-disable prettier/prettier */
+export enum Role {
+    ADMIN = 'admin',
+    MEMBER = 'member',
+  }
+export enum EvolutionStage {
+    LOWER = "zero",
+    FIRST = "one",
+    SECOND = "two",
+    THIRD = "three",
+    EVOLVED = 'evolved',
+    LOWERCASE = 'lower',
+}
+export enum Type {
+    GRASS = 'grass',
+    FIRE = 'fire',
+    WATER = 'water',
+    BUG = 'bug',
+    NORMAL = 'normal',
+    POISON = 'poison',
+    ELECTRIC = 'electric',
+    GROUND = 'ground',
+    FAIRY = 'fairy',
+    FIGHTING = 'fighting',
+    PSYCHIC = 'psychic',
+    ROCK = 'rock',
+    GHOST = 'ghost',
+    ICE = 'ice',
+    DRAGON = 'dragon',
+    DARK = 'dark',
+    STEEL = 'steel',
+    flying = 'flying'
+}
+export enum Weather {
+    SUNNY_CLEAR = 'Sunny/clear',
+    RAINY = 'Rainy',
+    PARTLY_CLOUDY = 'Partly cloudy',
+    CLOUDY = 'Cloudy',
+    WINDY = 'Windy',
+    FOG = 'Fog',
+    SNOW = 'Snow',
+}
---
+++ src/Users/DTO/create-user.dto.ts
@@ -0,0 +1,39 @@
+import { IsEmail, IsString, IsEnum, IsNotEmpty, MinLength } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import {Role} from "../../Shared/enums"
+// Create a Data Transfer Object (DTO) for creating a user
+export class CreateUserDto {
+  @ApiProperty({
+    example: 'user@example.com',
+    description: 'Email address for the user',
+  })
+  @IsEmail()
+  @IsNotEmpty()
+  readonly email: string;
+
+  @ApiProperty({
+    example: 'John Doe',
+    description: 'Name of the user',
+  })
+  @IsNotEmpty()
+  @IsString()
+  readonly name: string;
+
+  @ApiProperty({
+    example: 'password', 
+    description: 'The new user password (optional, min length: 7)',
+  })
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(7)
+  readonly password: string;
+
+  @ApiProperty({
+    enum: Role,
+    description: 'Role of the user (ADMIN or MEMBER)',
+  })
+  @IsNotEmpty()
+  @IsEnum(Role)
+  readonly role: Role;
+}
+
