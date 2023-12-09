8ef5a80292ef3f0dcabd6b0355439fb4a6fbf6cd
OdaiJawabreh <odaijawabreh.oj@gmail.com>
2023-12-09 19:43:52 +0300

    implement swagger to the main ts

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 import * as dotenv from 'dotenv';
 
 async function bootstrap() {
@@ -7,6 +9,20 @@ async function bootstrap() {
   const port = process.env.PORT || 3000;
 
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Pokemon APIs')
+    .setDescription('APIs for managing Pokemon data')
+    .setVersion('1.0')
+    .addTag('Pokemon')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs-pokemon-api', app, document);
+
+  // Enable validation globally
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
   await app.listen(port);
 }
 bootstrap();
