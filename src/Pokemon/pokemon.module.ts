753740948acb8fe594cf86e1b7bdd58762568586
OdaiJawabreh <odaijawabreh.oj@gmail.com>
2023-12-11 12:29:32 +0300

    Update Imports to run on ubinto system

--- src/Pokemon/pokemon.module.ts
+++ src/Pokemon/pokemon.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { PokemonController } from './pokemon.controller';
 import { PokemonService } from './pokemon.service';
-import { SharedModule } from 'src/shared/shared.module';
+import { SharedModule } from 'src/Shared/shared.module';
 
 
 @Module({
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { SharedModule } from 'src/shared/shared.module';
+import { SharedModule } from 'src/Shared/shared.module';
 
 @Module({
   controllers: [AuthController],
